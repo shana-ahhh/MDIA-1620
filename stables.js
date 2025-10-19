6868a9a6c566eaf2e5bcadce4df4f746b1f710e8
Shana <tluu18@my.bcit.ca>
2025-10-19 02:31:25 -0700

    Fixed lab3 after class

--- stables.js
+++ stables.js
@@ -1,70 +1,3 @@
-// Welcome!
-//
-//                                                  #    #
-//                                             %%% ##   ##
-//                                          %%%%% ###%🎀###
-//                                         %%%%% ### %%% #
-//                                       %%%%%% ### %%% ###
-//                                        %%%% ## %% #######
-//                                       %%%%% # %% #@#####
-//                                     %%%%%% # % #########
-//                                    %%%%% ##### #########
-//                          ###        %% ####### #########
-//                 %%% ############    ########### ########
-//              %%%% ############################### #######
-//            %%%%% ################################## ######
-//          %%%%%% #################################### #C###
-//         %%%🌸%% #####################################  ###
-//         %%%%% #######################################
-//        %%%%%% ############### BEANS ##################
-//     % %%%%%%% ############### BEANS ##################
-//      %%%%%%%%% ############## BEANS ##################
-//     %%%%%%%%%% ########################################
-//  %%% %%%%%%%%   ###### ################################
-//    %%%%%%%%      ###### #################### ##########
-// % %%%🌸%%%%        ####### ########### ###### ##########
-//  %%%%%%%%%         #######  ########### ###### ########
-// %%%%%%%%%%          ##### ###  ######### ####### ######
-//  %%%%%%%%%%          #### ##               ####### ####
-//  %🌸%%%%%%%%%           ## #                  ##### ###
-//   %%  %% % %%         # ##                      ## ###
-//     %   %    %        # ###                      # ###
-//                        # ###                     ## ###
-//                        # ###                     ## ###
-//                        # ####                   #### ##
-//                       ### ###                  ##### ###
-//                      ####  ###                 ####   ##
-//                     #####   ###                 ##    ##
-//                    #####    ####                      ###
-//                     ##        ###                     ###
-//                                ####                     ##
-//                                 ####                    ###
-//                                                         ####
-//                                                          ##
-//
-// This is a template for your labs and final project. You'll be setting up a stable and making
-// sure it runs smoothly.
-//
-// Please do not create a new file for each lab!
-// Feel free to keep the headings (e.g.: "Variables"), but please delete any boilerplate
-// comments. (Your own comments, where necessary, are acceptable.)
-//
-// NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
-// NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
-
-//------------------------- Seting up shop (Lab #1a, Week 3) -------------------------//
-
-// Declare variables with values for the following:
-// - the name of the horse at the stable
-// - the age of the horse
-// - whether the horse is inside or outside
-// - the cost to board the horse monthly
-// - the fee for a late payment (monthly rate + 20%)
-//
-// Create a variable and use it to store a message for visitors to the stable.
-// Create a variable and use it to store a message that monthly payment is late, and the amount owing.
-// Include the name of your horse in the message.
-
 let horseName = "Jelly";
 let horseAge = "5";
 let isHorseInside = true;
@@ -77,32 +10,11 @@ let latePaymentMessage =
     horseName +
     " is " +
     monthlyFee +
-    "." +
-    " The fee due to late monthly payment is " +
+    ". The fee due to late monthly payment is " +
     latePaymentFee +
-    "." +
-    "The total fee is " +
+    ". The total fee is " +
     (monthlyFee + latePaymentFee) +
     ".";
-
-//------------------------- First day (Lab #1b, Week 4) -------------------------//
-
-// Using an object, add at least 3 horses to your stables.
-//
-// The horses should have the following properties:
-// - name, nickname, favorite treat (string)
-// - age, monthly rent (number)
-// - location (boolean inside/outside)
-// - two unique properties of your choice (use any primitive)
-
-// Store the horses you've just created in a "horses" variable.
-// Keep your old horse info from week one for now.
-
-// Fancy! Another horse wants to be stabled! Create a variable that stores an object
-// literal of your new horse, and add it to your "horses" variable.
-
-// Initialize new property to your horses: a boolean that allows you to check if your horse
-// is hungry or not. Use dot notation.
 
 let horses = {
     horseOne: {
@@ -111,7 +23,7 @@ let horses = {
         favTreat: "candy",
         age: 3,
         monthlyRent: monthlyFee,
-        isOutside: true,
+        isInside: isHorseInside,
         colour: "black",
         trainingLevel: "beginner",
     },
@@ -121,7 +33,7 @@ let horses = {
         favTreat: "peanut",
         age: 7,
         monthlyRent: 130,
-        isOutside: false,
+        isInside: false,
         colour: "brown",
         trainingLevel: "advanced",
     },
@@ -131,7 +43,7 @@ let horses = {
         favTreat: "chips",
         age: 10,
         monthlyRent: 155,
-        isOutside: true,
+        isInside: true,
         colour: "white",
         trainingLevel: "intermediate",
     },
@@ -143,7 +55,7 @@ let newHorse = {
     favTreat: "potato",
     age: 4,
     monthlyRent: 80,
-    isOutside: false,
+    isInside: false,
     colour: "yellow",
     trainingLevel: "beginner",
 };
@@ -155,39 +67,35 @@ horses.horseTwo.isHungry = false;
 horses.horseThree.isHungry = false;
 horses.horseFour.isHungry = true;
 
-//------------------------- Growing our business (Lab #2, Week 6) -------------------------//
+const totalStalls = 10;
 
-// Create a variable that stores your total number of stalls.
-// Create a function that logs out how many stalls are available, given how many horses you have in your stable.
-
-// Create a function that logs out how much will an individual horse will owe if rent is
-// paid late. Invoke the function.
-
-// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
-// the return value outside of the function.
-
-let totalStalls = 15;
-function availableStalls() {
-    let totalOfHorses = 4;
-    console.log(totalStalls - totalOfHorses + " stalls are available.");
+function availableStalls(numberOfStalls, numberOfHorses) {
+    let numberOfAvailableStalls = numberOfStalls - numberOfHorses;
+    console.log("There are " + numberOfAvailableStalls + " stalls available!");
 }
 
-availableStalls();
-
-function owedRent() {
+function latePaymentNotice(horse) {
     console.log(
-        horses.horseOne.name +
-            " will owe $" +
-            latePaymentFee +
-            " if rent is paid late."
+        "Your payment for " +
+            horse.name +
+            " is late! You owe $" +
+            horse.monthlyRent * 0.2 +
+            " plus the monthly fee of $" +
+            horse.monthlyRent +
+            "!"
     );
 }
 
-owedRent();
-
 function chosenHorse(horse) {
-    return horse.nickname;
+    return (
+        "This is " +
+        horse.name +
+        ", you can also call him/her " +
+        horse.nickname +
+        "!"
+    );
 }
 
-let horseNickname = chosenHorse(horses.horseThree);
-console.log(horses.horseThree.name + "'s nickname is " + horseNickname + "!");
+availableStalls(totalStalls, 4);
+latePaymentNotice(horses.horseOne);
+console.log(chosenHorse(horses.horseTwo));
